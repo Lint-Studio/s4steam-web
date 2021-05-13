a0ec6e386a5e5eae248863abc53a5160f6a4f888
kingskell <ydeany500@gmail.com>
2021-05-13 13:16:15 +0000

    Initial Commit - nextjs setup

---
+++ pages/GlobalComponents/MyNavbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class MyNavbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <div className='mobile-nav-wrapper'>
+            
+        </div>);
+    }
+}
+ 
+export default MyNavbar;
